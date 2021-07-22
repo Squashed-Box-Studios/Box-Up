ca6476bc9b451f40b73b6e35e3284f2490f8de05
Squashed Box <86600974+Squashed-Box-Studios@users.noreply.github.com>
2021-07-22 12:47:50 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
-
-if(localStorage.clicks){
-    var clicks = localStorage.getItem("clicks");
-    document.getElementById("clicks").innerHTML = clicks;
+var i = 0
+const au = document.getElementById("audio")
+if(localStorage.punches){
+    var punches = localStorage.getItem("punches");
+    document.getElementById("punches").innerHTML = punches;
 } else {
-    var clicks = 0
+    var punches = 0
 }
-function c(){
-    clicks++
-    document.getElementById("clicks").innerHTML = clicks;
-    console.log(document.getElementById("clicks").innerHTML);
-    localStorage.setItem("clicks", clicks);
+if(localStorage.interval){
+    var punches = localStorage.getItem("interval");
+} else {
+    var interval = 1
+}
+function p(){
+    if (i == 0){
+        punches++
+        document.getElementById("punches").innerHTML = punches;
+        console.log(document.getElementById("punches").innerHTML);
+        localStorage.setItem("punches", punches);
+        au.play();
+        progress();
+    }
+}
+function progress(){
+    i = 1
+    var bar = document.getElementById("bar");
+    bar.style.backgroundColor = "#213781";
+    var width = 0;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+        bar.style.backgroundColor = "#2eff69";
+      } else {
+        width+= interval;
+        bar.style.width = width + "%";
+      }
+    }
 }
