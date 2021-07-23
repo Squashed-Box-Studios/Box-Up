692ad628d3d93de0b32b41f1078fbb7cc978ba77
Squashed Box <86600974+Squashed-Box-Studios@users.noreply.github.com>
2021-07-23 12:55:42 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,48 @@
 var i = 0
 const au = document.getElementById("audio")
 if(localStorage.punches){
-    var punches = localStorage.getItem("punches");
+    var punches = Number(localStorage.getItem("punches"))
     document.getElementById("punches").innerHTML = punches;
 } else {
     var punches = 0
+    var deci = 0
+}
+if(localStorage.deci){
+    var deci = Number(localStorage.getItem("deci"))
+} else {
+    var deci = 0
 }
 if(localStorage.interval){
-    var punches = localStorage.getItem("interval");
+    var interval = Number(localStorage.getItem("interval"))
 } else {
     var interval = 1
 }
+if(localStorage.strength){
+    var strength = Number(localStorage.getItem("strength"))
+    document.getElementById("strength").innerHTML = strength;
+} else {
+    var strength = 0
+}
+if(localStorage.speed){
+    var speed = Number(localStorage.getItem("speed"))
+    document.getElementById("speed").innerHTML = speed;
+} else {
+    var speed = 0
+}
 function p(){
     if (i == 0){
-        punches++
+        deci+= (strength / 100) + 1
+        punches = Math.round(deci);
         document.getElementById("punches").innerHTML = punches;
         console.log(document.getElementById("punches").innerHTML);
         localStorage.setItem("punches", punches);
+        localStorage.setItem("deci", deci);
         au.play();
         progress();
     }
 }
 function progress(){
+    interval = (speed / 100) + 1
     i = 1
     var bar = document.getElementById("bar");
     bar.style.backgroundColor = "#213781";
@@ -36,5 +57,27 @@ function progress(){
         width+= interval;
         bar.style.width = width + "%";
       }
+    }
+}
+function st(strengthp, punchesm){
+    if(punches >= punchesm){
+        Number(strength+= strengthp)
+        Number(deci-= punchesm)
+        Number(punches = Math.round(deci))
+        document.getElementById("punches").innerHTML = punches;
+        localStorage.setItem("punches", punches);
+        document.getElementById("strength").innerHTML = strength;
+        localStorage.setItem("strength", strength);
+    }
+}
+function sp(speedp, punchesm){
+    if(punches >= punchesm){
+        Number(speed+= speedp)
+        Number(deci-= punchesm)
+        Number(punches = Math.round(deci))
+        document.getElementById("punches").innerHTML = punches;
+        localStorage.setItem("punches", punches);
+        document.getElementById("speed").innerHTML = speed;
+        localStorage.setItem("speed", speed);
     }
 }
